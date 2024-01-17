05f01cdb92ca11f9946adf9f52eee3a1d010ff08
gaurav2503b <gaurav.b@simformsolutions.com>
2024-01-17 10:30:03 +0530

    added table and done some configuration changes in the application

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  routeRules: {
+    // with route rules you can tell which particular page should be rendered in client or server rendering mode
+    '/': { ssr: true},
+    '/client': {ssr: false}
+  },
+  css: ["~/assets/css/main.scss"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
